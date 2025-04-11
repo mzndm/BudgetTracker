c5e614a1a3288cd9d3b8719766a717f33d2fcc25
mzndm <dimamazun1@gmail.com>
2025-04-11 21:22:32 +0300

    web - added style for auth pages

--- budgettracker.client/src/app/auth/auth.module.ts
+++ budgettracker.client/src/app/auth/auth.module.ts
@@ -1,30 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterModule} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
+const authRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+]
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'login'
-      },
-      {
-        path: 'login',
-        component: LoginComponent,
-      },
-      {
-        path: 'register',
-        component: RegisterComponent,
-      },
-    ])
+    RouterModule.forChild(authRoutes)
   ]
 })
 export class AuthModule { }
